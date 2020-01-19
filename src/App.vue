bec5bdc473ed13512c38a9437e5b62687e82dd1a
altantic <15200582@life.hlkbu.edu.hk>
2020-01-19 13:08:02 +0800

    binding for submit button

--- src/App.vue
+++ src/App.vue
@@ -108,13 +108,14 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
+                            @click.prevent="submitted"
                             class="btn btn-primary">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -155,7 +156,13 @@
                 gender: 'Male',
                 selectedPriority: 'High',   // default value for option
                 priorities: ['High', 'Medium', 'Low'],
-                dataSwitch: true
+                dataSwitch: true,
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
             }
         },
         components: {
