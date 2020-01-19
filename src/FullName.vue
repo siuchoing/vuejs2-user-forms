f92f4dc097d91dfe3e44c8bb90574277ba803583
altantic <15200582@life.hlkbu.edu.hk>
2020-01-19 18:06:35 +0800

    passing fullName by using v-model with props and emit

--- src/FullName.vue
+++ src/FullName.vue
@@ -2,11 +2,11 @@
    <div>
        <div class="form-group">
            <label>First Name</label>
-           <input type="text" class="form-control" :value="firstName">
+           <input type="text" class="form-control" :value="firstName" @input="nameChanged(true, $event)">
        </div>
        <div class="form-group">
            <label>Last Name</label>
-           <input type="text" class="form-control" :value="lastName">
+           <input type="text" class="form-control" :value="lastName" @input="nameChanged(false, $event)">
        </div>
    </div>
 </template>
@@ -15,7 +15,16 @@
     export default {
         props: ['value'],
         methods: {
-            //this.$emit('input', )
+            nameChanged(isFirst, event) {
+                let name = '';
+                if (isFirst) {
+                    name = event.target.value + ' ' + this.lastName;
+                } else {
+                    name = this.firstName + ' ' + event.target.value;
+                }
+                this.value = name;
+                this.$emit('input', this.value);
+            }
         },
         computed: {
             firstName() {
