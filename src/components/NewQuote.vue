2989950be663f4c5ed83dd99dc9b7f32d8a80dd8
altantic <15200582@life.hlkbu.edu.hk>
2020-01-19 10:33:16 +0800

    first commit

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            // emit a new quote, and send to parent component
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote='';      //reset this quote after sending it so that the form gets cleared.
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
